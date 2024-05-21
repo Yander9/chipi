9de231718454a96001349607760f8d832374bcd3
Yander9 <160254914+Yander9@users.noreply.github.com>
2024-05-21 23:02:07 +0200

    Добавлена первая лаба

---
+++ Res1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+int main() {
+    float result;
+    using namespace std;
+    int r;
+
+    cin >> r;
+    float p = 3.14;
+    result = r * r * p;
+    cout << "Ploshad' kruga = " << result;
+
+}
